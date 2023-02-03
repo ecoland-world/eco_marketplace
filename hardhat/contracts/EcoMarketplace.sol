a25796e1fb8603c4bb5d1cba6fee7984b84b094c
cosisaxis <kishanelewis2@gmail.com>
2023-02-03 12:36:33 -0500

    structs/mappings

--- hardhat/contracts/EcoMarketplace.sol
+++ hardhat/contracts/EcoMarketplace.sol
@@ -2,5 +2,35 @@ pragma solidity 0.8.17;
 
 
 contract EcoMarketPlace{
-    
+
+    struct Bid{
+        uint256 value;
+        address contact;
+        address seller;
+        uint256 saleid;
+        address bidder;
+    }
+     struct Sale{
+        bool biddable;
+        uint256 price;
+        uint256 nftid;
+        uint256 amount;
+     }
+
+    struct ExtendedSale{
+        Sale sale;
+        uint256 HighestBid;
+        address contact;
+        address seller;
+    }
+
+    struct HighestBid{
+        uint256 value;
+        uint256 bidid;
+    }
+
+    mapping(address => mapping(address => mapping(uint256 => ExtendedSale))) sales;
+    mapping(uint256 => Bid) bids;
+    mapping(address => bool) admin;
+
 }
