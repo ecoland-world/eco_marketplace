136d041e530b038669c8b8d9f25e65270f8ccf9f
jovi <77895278+joaovwfreire@users.noreply.github.com>
2023-02-05 00:34:32 -0300

    smart contract interfaces

--- hardhat/contracts/EcoMarketplace.sol
+++ hardhat/contracts/EcoMarketplace.sol
@@ -1,7 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
 
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract EcoMarketPlace{
+
+contract EcoMarketPlace is Ownable, ReentrancyGuard{
 
     struct Bid{
         uint256 value;
@@ -29,8 +33,115 @@ contract EcoMarketPlace{
         uint256 bidid;
     }
 
-    mapping(address => mapping(address => mapping(uint256 => ExtendedSale))) sales;
-    mapping(uint256 => Bid) bids;
-    mapping(address => bool) admin;
+    address internal ngoAddress;
+    string internal version;
+    uint256 internal fee;
+    uint256 internal ngoBalance;
+    uint256 internal ownerBalance;
 
-}
+    mapping(address => mapping(address => mapping(uint256 => ExtendedSale))) public sales;
+    mapping(uint256 => Bid) public bids;
+    mapping(address => bool) public admin;
+
+    modifier onlyAdmin(){
+        require(admin[msg.sender], "Only Admin");
+        _;
+    }
+
+    function setFee() external onlyAdmin{
+        //TODO
+    }
+
+    function setNgoAddress() external onlyAdmin{
+        //TODO
+    }
+
+    function setAdmin() external onlyOwner{
+        //TODO
+    }
+
+    function removeAdmin() external onlyOwner{
+        //TODO
+    }
+
+    function setVersion() external onlyAdmin{
+        //TODO
+    }
+
+    function getSale() public view returns(ExtendedSale memory){
+        //TODO
+    }
+
+    function getBid() public view returns(Bid memory){
+        //TODO
+    }
+
+    function getHighestBid() public view returns(HighestBid memory){
+        //TODO
+    }
+
+    function getFee() public view returns(uint256){
+        //TODO
+    }
+
+    function getNgoAddress() public view returns(address){
+        //TODO
+    }
+
+    function isAdmin(address adminAddress) public view returns(bool){
+        //TODO
+    }
+
+    function getVersion() public view returns(string memory){
+        //TODO
+    }
+
+    function getOwner() public view returns(address){
+        //TODO
+    }
+
+    function getOwnerBalance() public view returns(uint256){
+        //TODO
+    }
+
+    function getNGOBalance() public view returns(uint256){
+        //TODO
+    }
+
+    function buy() external payable nonReentrant{
+        //TODO
+    }
+
+    function bid() external payable nonReentrant{
+        //TODO
+    }
+
+    function acceptBid() external nonReentrant{
+        //TODO
+    }
+
+    function createSale() external{
+        //TODO
+    }
+
+    function cancelSale() external {
+        //TODO
+    }
+
+    function cancelBid() external nonReentrant{
+        //TODO
+    }
+
+    function withdraw() external nonReentrant{
+        //TODO
+    }
+
+    function withdrawNgo() external nonReentrant{
+        //TODO
+    }
+
+    function withdrawOwner() external nonReentrant{
+        //TODO
+    }
+
+}    
