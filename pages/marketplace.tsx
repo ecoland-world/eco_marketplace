f09f00021363a0f5f55f5e1dfd49736f626443a1
Kerim <kerimberdi.agayev@edusoftech.eu>
2023-02-23 17:02:35 +0200

    show listings

--- pages/marketplace.tsx
+++ pages/marketplace.tsx
@@ -1,7 +1,55 @@
 import React from "react";
+import Link from "next/link";
+import {
+  useActiveListings,
+  useContract,
+  MediaRenderer,
+} from "@thirdweb-dev/react";
+import { useRouter } from "next/router";
 
 const Marketplace = () => {
-  return <div>Marketplace</div>;
+  const { contract } = useContract(
+    "0x3632b6971FAf78D32eD0e14C455CBC6882ced7F7",
+    "marketplace"
+  );
+
+  const { data: listings, isLoading: loadingListings } =
+    useActiveListings(contract);
+
+  const router = useRouter();
+
+  return (
+    <div>
+      {
+        // load listings
+        loadingListings ? (
+          <div>Loading listings...</div>
+        ) : (
+          // show listings
+          <div>
+            {listings?.map((listing) => (
+              <div
+                key={listing.id}
+                onClick={() => router.push(`/listing/${listing.id}`)}
+              >
+                <MediaRenderer src={listing.asset.image} />
+                <h2>
+                  <Link href={`/listing/${listing.id}`}>
+                    {listing.asset.name}
+                  </Link>
+                </h2>
+
+                <p>
+                  <b>{listing.buyoutCurrencyValuePerToken.displayValue}</b>{" "}
+                  {listing.buyoutCurrencyValuePerToken.symbol}
+                </p>
+              </div>
+            ))}
+          </div>
+        )
+      }
+    </div>
+  );
 };
 
 export default Marketplace;
